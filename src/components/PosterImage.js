b648e586a7948127ba12bfeaf4593a21327e9e32
pawan <pawan.dharel777@gmail.com>
2020-06-19 12:18:18 +0545

    poster and preview image added

--- src/components/PosterImage.js
+++ src/components/PosterImage.js
@@ -1,19 +1,42 @@
 import React from "react";
-import { StyleSheet, View, Text, Image } from "react-native";
+import { StyleSheet, View, Image, Dimensions } from "react-native";
+import { Text } from "react-native-paper";
 import { baseURL } from "../api/api";
 
-const PosterImage = ({ movie }) => {
-    console.log(movie)
+export const PosterImage = ({ movie }) => {
+  console.log(movie);
   return (
     <View>
       <Image
-        source={{ uri: `${baseURL}/uploads/${movie}` }}
-        style={{ height: 300, width: 300,zIndex:-1 }}
+        source={{ uri: `${baseURL}/uploads/${movie.image}` }}
+        style={{
+          width: Dimensions.get("window").width,
+          height: Dimensions.get("window").height * 0.6,
+          zIndex: -1,
+        }}
+        resizeMode="stretch"
       />
-      <Text>{movie}</Text>
+      <Text style={{ position: "absolute", bottom: 40 }}>{movie.name}</Text>
+    </View>
+  );
+};
+
+export const Preview = ({ movie }) => {
+  return (
+    <View>
+      <Image
+        source={{ uri: `${baseURL}/uploads/${movie.image}` }}
+        style={{
+          width: 100,
+          height: 100,
+          marginHorizontal:10,
+          marginVertical:20,
+          borderRadius: 50,
+          zIndex: -1,
+        }}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({});
-export default PosterImage;
--- src/screens/User/HomeScreen.js
+++ src/screens/User/HomeScreen.js
@@ -4,7 +4,7 @@ import { Text, Button } from "react-native-paper";
 import { useSelector, useDispatch } from "react-redux";
 import { Logout } from "../../store/actions/authAction";
 import HeaderComponent from "../../components/HeaderComponent";
-import PosterImage from "../../components/PosterImage";
+import { PosterImage, Preview } from "../../components/PosterImage";
 import Axios from "axios";
 import { useEffect } from "react";
 import { getMovie } from "../../store/actions/movieAction";
@@ -44,7 +44,16 @@ const HomeScreen = () => {
         horizontal
         keyExtractor={(items) => items._id}
         renderItem={({ item }) => {
-          return <PosterImage movie={item.image} />;
+          return <PosterImage movie={item} />;
+        }}
+      />
+<Text>Preview</Text>
+      <FlatList
+        data={data}
+        horizontal
+        keyExtractor={(items) => items._id}
+        renderItem={({ item }) => {
+          return <Preview movie={item} />;
         }}
       />
 
